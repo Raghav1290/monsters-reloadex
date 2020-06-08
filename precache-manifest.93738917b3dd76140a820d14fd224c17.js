7e2085bbaeef6b826b9a93507ca68d9f3abff031
Raghav Sharma <get2raghavsharma@gmail.com>
2020-06-08 13:32:43 +0530

    Updates

---
+++ precache-manifest.93738917b3dd76140a820d14fd224c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3505865e7eba4166062246f08c44f4",
+    "url": "/Raghav1290/monsters-reloadex/index.html"
+  },
+  {
+    "revision": "9e99783551a58e48de5f",
+    "url": "/Raghav1290/monsters-reloadex/static/css/main.84221ca5.chunk.css"
+  },
+  {
+    "revision": "6775cc3fcc7d0ba12bc7",
+    "url": "/Raghav1290/monsters-reloadex/static/js/2.f0116d2b.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Raghav1290/monsters-reloadex/static/js/2.f0116d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e99783551a58e48de5f",
+    "url": "/Raghav1290/monsters-reloadex/static/js/main.d7507dd8.chunk.js"
+  },
+  {
+    "revision": "8a5ba8a8f48fc96ba75a",
+    "url": "/Raghav1290/monsters-reloadex/static/js/runtime-main.697f9b09.js"
+  }
+]);
